ab33f27cbd0f651e78ffa02fbce3dcd08a6b7dd0
Patrick Brett <pkbrett37@gmail.com>
2020-01-11 12:48:47 +1100

    chanceydo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="container">
+  <input v-model="newName" placeholder="Name..." />
+  <input v-model="newPlace" placeholder="Place..." />
+
+  <select v-model="currentColour">
+    <option v-for="colour in colours">{{colour}}</option>
+  </select>
+
+  <button @click="addPerson">Add person</button>
+  <button @click="removeAll">Remove all</button>
+  <div class="person" v-for="(person, index) in people">
+    <div :class="'label ' + person.colour"></div>
+    <b>{{person.name}}</b> <i>{{person.place}}</i>
+    <button @click="removePerson(index)">remove</button>
   </div>
+  <div v-if="people.length === 0"> Add people here</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      newName: "",
+      newPlace: "",
+      people: [
+        {
+          name: "naomi",
+          place: "india",
+          colour: "green"
+        },{
+          name: "lidia",
+          place: "italy",
+          colour: "blue"
+        }
+      ],
+      colours: [ "red", "green", "blue" ],
+      currentColour: "red"
+    }
+  },
+  methods: {
+    addPerson() {
+      let newPerson = {
+        name: this.newName,
+        place: this.newPlace,
+        colour: this.currentColour
+      }
+      this.people.push(newPerson)
+      this.newName = ""
+      this.newPlace= ''
+    },
+    removePerson(index) {
+      this.people.splice(index,1)
+    },
+    removeAll(){
+      this.people=[]
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #container {
+    border: 3px solid black;
+    padding: 20px;
+    margin: 20px;
+    display: inline-block;
+  }
+
+  .person {
+    margin: 10px;
+  }
+
+  .label {
+    width: 20px;
+    height: 20px;
+    border-radius: 10px;
+    display: inline-block;
+  }
+
+  .red {
+    background: #c44031;
+  }
+
+  .blue {
+    background: #1580d1;
+  }
+
+  .green {
+    background: #14ba43;
+  }
 </style>
